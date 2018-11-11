40cd2b725b92dd97ca78826e6cd351010baa8b39
Max <maaax.fischer@gmail.com>
2018-11-11 19:55:00 +0100

    Add basic auth and request number argument

--- webpage_poke/run.py
+++ webpage_poke/run.py
@@ -1,26 +1,70 @@
 #!/usr/bin/env python
 
-import time
 import argparse
 import itertools
-
+import re
+import time
 from statistics import median
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from typing import List
+
 import matplotlib.pyplot as plt
+
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('url', metavar='URL', help='URL to poke', type=str)
+    parser.add_argument('-n', '--requests', metavar='N',
+                        help='number of requests', type=int, default=20)
+    parser.add_argument('-a', '--basic-auth', metavar='USER@PASSWORD',
+                        help='basic auth credentials', type=str)
     args = parser.parse_args()
 
-    start_url = args.url
-    timings = calculate_page_load_times(start_url)
-    create_statistics(timings, start_url)
+    url = build_url(args.url, args.basic_auth)
+    timings = calculate_page_load_times(url, args.requests)
+    plot_statistics(timings, args.url)
 
 
-def create_statistics(timings, start_url):
+def build_url(url: str, basic_auth: str):
+    # type: (str, str) -> str
+    if basic_auth:
+        url = re.sub(r'^(http://|https://|)', r'\1' +
+                     basic_auth + '@', url, count=1)
+    return url
+
+
+def calculate_page_load_times(url, requests):
+    # type: (str, int) -> List[float]
+    timings = []
+    query_parameter_creation_pool = 'xyz2456'
+
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('network.http.phishy-userpass-length', 255)
+    binary = FirefoxBinary('/usr/bin/firefox-developer-edition')
+    driver = webdriver.Firefox(firefox_binary=binary, firefox_profile=profile)
+
+    for i in range(requests):
+        # (n+r-1)! / r! / (n-1)! combinations where r = 7 here
+        query_params = list(itertools.combinations_with_replacement(
+            query_parameter_creation_pool, 7))
+        # length of query_params should always be > n
+        tmp_url = f'{url}?{"".join(query_params[i])}'
+        start = time.time()
+        # driver.implicitly_wait(10)  # seconds
+        driver.get(tmp_url)
+        wait_for_ajax(driver)
+        end = time.time()
+        timings.append(end - start)
+
+    driver.close()
+    return timings
+
+
+def plot_statistics(timings, start_url):
+    # type: (int, str) -> None
     avg_time = sum(timings) / float(len(timings))
     median_time = median(timings)
     n, bins, patches = plt.hist(
@@ -33,34 +77,12 @@ def create_statistics(timings, start_url):
     plt.show()
 
 
-def calculate_page_load_times(start_url, n=20):
-    timings = []
-    query_parameter_creation_pool = 'xyz2456'
-    browser = webdriver.Firefox()
-
-    for i in range(n):
-        # (n+r-1)! / r! / (n-1)! combinations where r = 7 here
-        query_params = list(itertools.combinations_with_replacement(
-            query_parameter_creation_pool, 7))
-        # length of query_params should always be > n
-        tmp_url = f'{start_url}?{"".join(query_params[i])}'
-        start = time.time()
-        # browser.implicitly_wait(10)  # seconds
-        browser.get(tmp_url)
-        wait_for_ajax(browser)
-        end = time.time()
-        timings.append(end - start)
-
-    browser.close()
-    return timings
-
-
-def wait_for_ajax(browser):
-    wait = WebDriverWait(browser, 15)  # 15 seconds
+def wait_for_ajax(driver):
+    wait = WebDriverWait(driver, 15)  # 15 seconds
     try:
-        wait.until(lambda browser: browser.execute_script(
+        wait.until(lambda driver: driver.execute_script(
             'return jQuery.active') == 0)
-        wait.until(lambda browser: browser.execute_script(
+        wait.until(lambda driver: driver.execute_script(
             'return document.readyState') == 'complete')
     except Exception as e:
         pass
